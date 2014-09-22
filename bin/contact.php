a9eabb1bca118fb0fa5ffe1c4d95787ab2d12d30
Miek Gieben <miek@miek.nl>
2014-09-22 20:26:24 +0000

    more

---
+++ bin/contact.php
@@ -0,0 +1,26 @@
+<?php
+// check if fields passed are empty
+if(empty($_POST['inputName'])  		||
+   empty($_POST['inputEmail']) 		||
+   empty($_POST['inputMessage'])	||
+   !filter_var($_POST['inputEmail'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+#	return false;
+   }
+
+$name = $_POST['inputName'];
+$email = $_POST['inputEmail'];
+$message = $_POST['inputMessage'];
+
+// create email body and send it	
+$to = 'miek@miek.nl';
+$email_subject = "Contact form submitted by:  $name";
+$email_body = "You have received a new message. \n\n".
+				  " Here are the details:\n \nName: $name \n ".
+				  "Email: $email\n Message \n $message";
+$headers = "From: contacts@myprogrammingblog.com\n";
+$headers .= "Reply-To: $email";	
+#mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
