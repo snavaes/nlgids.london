141fead85ecd4fda69320870c75ae1cd5d4ddea2
Miek Gieben <miek@miek.nl>
2014-09-28 10:15:40 +0000

    Fix all the forms

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,9 @@
 /*
+ * Loosy javascript experiments.
   Jquery Validation using jqBootstrapValidation
 */
 $(function() {
- $("input,textarea").jqBootstrapValidation(
+ $("input[name='inputBooking'],textarea[name='inputBooking']").jqBootstrapValidation(
     {
      preventSubmit: true,
      submitError: function($form, event, errors) {
@@ -12,18 +13,13 @@ $(function() {
      submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
        // get values from FORM
-       var name = $("input#inputName").val();
-       var email = $("input#inputEmail").val();
-       var size = $("input#inputSize").val();
-       var tour = $("input#inputTour").val();
-       var date = $("input#inputDate").val();
-       var message = $("textarea#inputMessage").val();
+       var nameBooking = $("input#inputBookingName").val();
+       var emailBooking = $("input#inputBookingEmail").val();
+       var sizeBooking = $("input#inputBookingSize").val();
+       var tourBooking = $("input#inputBookingTour").val();
+       var dateBooking = $("input#inputBookingDate").val();
+       var messageBooking = $("textarea#inputBookingMessage").val();
        var formtype = $("input#formContactType").val();
-       // Stupid checks for the other forms.
-       if (tour == '') { tour = $("select#inputTour").val(); }
-       if (name == '') { name = $("input#inputContactName").val(); }
-       if (message == '') { message = $("textarea#inputContactMessage").val(); }
-       if (formtype == '') { formtype = $("input#formNavType").val(); }
 
        langcookie=document.cookie;
 
@@ -35,7 +31,7 @@ $(function() {
 	 $.ajax({
                 url: "../bin/contact.php",
             	type: "POST",
-            	data: {formType: formtype, inputName: name, inputEmail: email, inputSize: size, inputDate: date, inputTour: tour, inputMessage: message},
+            	data: {formType: formtype, inputName: nameBooking, inputEmail: emailBooking, inputSize: sizeBooking, inputDate: dateBooking, inputTour: tourBooking, inputMessage: messageBooking},
             	cache: false,
             	success: function() {
             	// Success message
@@ -105,13 +101,15 @@ $('#myModal').on('shown.bs.modal', function () {
       $('#inputSize').attr('placeholder', placeholder("Aantal personen"));
       $('#inputName').attr('placeholder', placeholder("Naam"));
       $('#inputMessage').attr('placeholder', placeholder("Extra vragen/opmerkingen"));
+      $('#inputName').focus();
 });
 
 $('#myNav').on('shown.bs.modal', function () {
-      $('#inputDate').attr('placeholder', placeholder("  Datum"));
-      $('#inputSize').attr('placeholder', placeholder("Aantal personen"));
-      $('#inputName').attr('placeholder', placeholder("Naam"));
-      $('#inputMessage').attr('placeholder', placeholder("Extra vragen/opmerkingen"));
+      $('#inputNavDate').attr('placeholder', placeholder("  Datum"));
+      $('#inputNavSize').attr('placeholder', placeholder("Aantal personen"));
+      $('#inputNavName').attr('placeholder', placeholder("Naam"));
+      $('#inputNavMessage').attr('placeholder', placeholder("Extra vragen/opmerkingen"));
+      $('#inputNavName').focus();
 });
 
 $('#myContact').on('shown.bs.modal', function () {
@@ -141,7 +139,7 @@ function placeholder(text) {
 
 $(document).ready(function() {
       $(".announce").click(function() { // Click to only happen on announce links
-          $("#inputTour").val($(this).data('id'));
+          $("#inputBookingTour").val($(this).data('id'));
       });
 });
 
@@ -171,6 +169,86 @@ $(function() {
                 url: "../bin/contact.php",
             	type: "POST",
             	data: {formType: formtype, inputName: nameContact, inputEmail: emailContact, inputMessage: messageContact},
+            	cache: false,
+            	success: function() {
+            	// Success message
+            	   $('.success').html("<div class='alert alert-success'>");
+            	   $('.success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            		.append( "</button>");
+                    if ( langcookie == '' || langcookie == 'lang=nl' ) {
+                          $('.success > .alert-success')
+                                .append("<strong>Bericht is met succes verzonden. </strong>");
+                    } else {
+                          $('.success > .alert-success')
+                                .append("<strong>Message has been successfully sent. </strong>");
+                    }
+ 		  $('.success > .alert-success')
+ 			.append('</div>');
+
+ 		  //clear all fields
+ 		  $('#contactForm').trigger("reset");
+ 		  $('#navForm').trigger("reset");
+ 		  $('#bookForm').trigger("reset");
+ 	      },
+ 	   error: function() {
+ 		// Fail message
+ 		 $('.success').html("<div class='alert alert-danger'>");
+            	$('.success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            	 .append( "</button>");
+                    if ( langcookie == '' || langcookie == 'lang=nl' ) {
+                        $('.success > .alert-danger').append("<strong>Sorry "+firstName+", er lijkt iets mis te gaan...</strong> Kunt u een directe mail sturen naar <a href='mailto:ans@nlgids.london?Subject=[NLgids] reservering'>ans@nlgids.london</a> ? Sorry voor het ongemak!");
+                    } else {
+                        $('.success > .alert-danger').append("<strong>Sorry "+firstName+", something has gone wrong...</strong> Could you send a direct mail to <a href='mailto:ans@nlgids.london?Subject=[NLgids] booking'>ans@nlgids.london</a> ? Sorry the trouble!");
+                    }
+ 	        $('.success > .alert-danger').append('</div>');
+ 		//clear all fields
+ 		$('#contactForm').trigger("reset");
+ 		$('#navForm').trigger("reset");
+ 		$('#bookForm').trigger("reset");
+ 	    },
+           })
+         },
+         filter: function() {
+                   return $(this).is(":visible");
+         },
+       });
+
+      $("a[data-toggle=\"tab\"]").click(function(e) {
+                    e.preventDefault();
+                    $(this).tab("show");
+        });
+  });
+
+
+$(function() {
+ $("input[name='inputNav'],textarea[name='inputNav'],select[name='inputNav']").jqBootstrapValidation(
+    {
+     preventSubmit: true,
+     submitError: function($form, event, errors) {
+      // something to have when submit produces an error ?
+      // Not decided if I need it yet
+     },
+     submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+       // get values from FORM
+       var nameNav = $("input#inputNavName").val();
+       var emailNav = $("input#inputNavEmail").val();
+       var messageNav = $("textarea#inputNavMessage").val();
+       var sizeNav = $("input#inputNavSize").val();
+       var tourNav = $("select#inputNavTour").val();
+       var dateNav = $("input#inputNavDate").val();
+       var formtype = $("input#formNavType").val();
+       langcookie=document.cookie;
+
+       var firstName = name; // For Success/Failure Message
+           // Check for white space in name for Success/Fail message
+        if (firstName.indexOf(' ') >= 0) {
+	   firstName = name.split(' ').slice(0, -1).join(' ');
+         }
+	 $.ajax({
+                url: "../bin/contact.php",
+            	type: "POST",
+            	data: {formType: formtype, inputName: nameNav, inputEmail: emailNav, inputSize: sizeNav, inputDate: dateNav, inputTour: tourNav, inputMessage: messageNav},
             	cache: false,
             	success: function() {
             	// Success message
