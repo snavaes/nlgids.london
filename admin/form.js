068768bea043d766f9345257d8dfe169d9ff5505
Miek Gieben <miek@miek.nl>
2015-12-26 16:47:31 +0000

    more

--- admin/form.js
+++ admin/form.js
@@ -1,31 +1,37 @@
 $('#adminTour').change(function () {
     switch ($('#adminTour').val()) {
         case "Tour op Maat":
-            $('#adminWhere').val("A");
+            $('#adminWhere').val("");
+            $('#adminHow').val("Ik sta daar om");
             break;
         case "Van Koninklijke Huize":
-            $('#adminWhere').val("B");
+            $('#adminWhere').val("bij de uitgang van Metrostation Westminster (bij het standbeeld van Boudicca, een woeste dame met paarden, naast de brug)");
+            $('#adminHow').val("Ik sta daar om");
             break;
         case "Romeinen en Bankiers":
-            $('#adminWhere').val("C");
+            $('#adminWhere').val("");
+            $('#adminHow').val("Ik sta daar om");
             break;
         case "Veldslagen en de Blitz":
-            $('#adminWhere').val("D");
+            $('#adminWhere').val("");
+            $('#adminHow').val("Ik sta daar om");
             break;
         case "Vertier Toen en Nu":
-            $('#adminWhere').val("E");
+            $('#adminWhere').val("");
+            $('#adminHow').val("Ik sta daar om");
             break;
         case "Muziek en Theater":
-            $('#adminWhere').val("F");
+            $('#adminWhere').val("");
+            $('#adminHow').val("Ik sta daar om");
             break;
         case "Nederlanders in de City":
-            $('#adminWhere').val("G");
+            $('#adminWhere').val("");
+            $('#adminHow').val("Ik sta daar om");
             break;
         case "Fietstour Londen":
-            $('#adminWhere').val("H");
-            break;
         case "Fietstour Secret London":
-            $('#adminWhere').val("I");
+            $('#adminWhere').val("Waterloo treinstation, spoor 1/2");
+            $('#adminHow').val("Ik kom jullie met de fiets ophalen om");
             break;
     }
 });
