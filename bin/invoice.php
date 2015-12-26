d37bcb535a11554f2ef60a9627717a050d889006
Miek Gieben <miek@miek.nl>
2015-12-26 13:06:14 +0000

    almost working

--- bin/invoice.php
+++ bin/invoice.php
@@ -13,14 +13,27 @@ $adminWhere   = $_POST["adminWhere"];
 $adminHow     = $_POST["adminHow"];
 
 if (empty($adminTour) || empty($adminCost)) {
-    error_log("nlgids PDF: tour and cost is empty");
+    error_log("nlgids PDF: tour or cost is empty");
 	return false;
 }
 
-# export as environment variables can call replace
-
 # put in log as fast as possible.
 error_log("nlgids PDF: $adminTour, $adminPersons, $adminTime, $adminDuration, $adminCost, $adminDate, $adminName, $adminFullName, $adminEmail,  $adminWhere, $adminHow");
+
+# export as environment variables can call replace
+putenv("IN_TOUR=$adminTour");
+putenv("IN_PERSONS=$adminPersons");
+putenv("IN_TIME=$adminTime");
+putenv("IN_DURATION=$adminDuration");
+putenv("IN_COST=$adminCost");
+putenv("IN_DATE=$adminDate");
+putenv("IN_NAME=$adminName");
+putenv("IN_FULLNAME=$adminFullName");
+putenv("IN_EMAIL=$adminEmail");
+putenv("IN_WHERE=$adminWhere");
+putenv("IN_HOW=$adminHow");
+
+exec('/home/miek/tex/invoice/replace.sh');
 
 return true;
 ?>
