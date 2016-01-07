3a1403e5d3cf139d7803cd871b1ee502adf42d68
Miek Gieben <miek@miek.nl>
2016-01-07 09:31:01 +0000

    errors here are not propagated back

--- bin/invoice.php
+++ bin/invoice.php
@@ -12,11 +12,6 @@ $adminEmail   = $_POST["adminEmail"];
 $adminWhere   = $_POST["adminWhere"];
 $adminHow     = $_POST["adminHow"];
 
-if (empty($adminTour) || empty($adminCost)) {
-    error_log("nlgids PDF: tour or cost is empty");
-	return false;
-}
-
 # put in log as fast as possible.
 error_log("nlgids PDF: $adminTour, $adminPersons, $adminTime, $adminDuration, $adminCost, $adminDate, $adminName, $adminFullName, $adminEmail,  $adminWhere, $adminHow");
 
