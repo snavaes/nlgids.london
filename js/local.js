651b168611c1652948f0e0203f040b66581493dd
Miek Gieben <miek@miek.nl>
2016-01-31 15:00:55 +0000

    error handler

--- js/local.js
+++ js/local.js
@@ -8,22 +8,26 @@ $("#formTest").submit(function(event){
 // The success will be called on id-success and error on id-error.
 function PostForm(id, action) {
     datastring = $(id).serialize();
-
     $.ajax({
         type: "POST",
         url: action,
         data: datastring,
         success: PostSuccess(id+"-success"),
-        error: function(){
-            // #formTest-error
-            alert('error handing here');
-        }
+        error: PostError(id+"-success"),
     });
+    $(id).trigger("reset");
 }
 
 function PostSuccess(id) {
     $(id).html("<div class='alert alert-success'>");
-    $(id).html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append( "</button>");
-    $(id).append("<strong>Message has been successfully sent.</strong>");
+    $(id + "> .alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append( "</button>");
+    $(id + "> .alert-success").append("<strong>Message has been successfully sent.</strong>");
+    $(id).append('</div>');
+}
+
+function PostError(id) {
+    $(id).html("<div class='alert alert-danger'>");
+    $(id + "> .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append( "</button>");
+    $(id + "> .alert-danger").append("<strong>Something went wrong.</strong>");
     $(id).append('</div>');
 }
