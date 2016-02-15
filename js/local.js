ee2ac12dab89b5c9cb0d955087feef4f8c48b6dc
Miek Gieben <miek@miek.nl>
2016-02-15 21:10:33 +0100

    stuff

--- js/local.js
+++ js/local.js
@@ -25,6 +25,18 @@ $('.modal').on('hidden.bs.modal', function(){
     $("#formBooking")[0].reset();
     $("#formBooking-success").html("");
 });
+/* On booking's form show, get the calendar and set html() and show */
+$('#exampleModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+  modal.find('.modal-body input').val(recipient)
+})
+
+/* Get calendar from buttons */
 
 /*
  * Functions below
