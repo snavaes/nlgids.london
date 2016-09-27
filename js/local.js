6bc6e58b3ed00895f164e61d957a7f44cfb66f72
Miek Gieben <miek@miek.nl>
2016-09-27 21:46:09 +0100

    make error function as well

--- js/local.js
+++ js/local.js
@@ -32,6 +32,8 @@ $("#formInvoice").submit(function(e){
     }
     PostForm("#formInvoice", "/api/auth/invoice");
 });
+
+
 /* Clear content on hide */
 $('.modal').on('hidden.bs.modal', function(){
     $("#formContact").trigger("reset");
@@ -160,7 +162,9 @@ function PostForm(id, action) {
             PostSuccess(id+"-success");
             $(id).trigger("reset");
         },
-        error: PostError(id+"-success"),
+        error: function() {
+            PostError(id+"-success")l
+        },
     });
 }
 
